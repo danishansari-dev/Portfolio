84c3907534f71bb1523f711d16691d5b1e6f3c61
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2026-03-22 01:49:05 +0530

    chore: re-add gitignore

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// Vite config with React and Tailwind CSS v4 plugin
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
